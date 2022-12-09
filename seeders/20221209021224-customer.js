7473cb849e200d80f26d2f66e684399aa41eb5c4
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-08 21:46:47 -0500

    restaurant seed created

---
+++ seeders/20221209021224-customer.js
@@ -0,0 +1,25 @@
+'use strict'
+const falso = require('@ngneat/falso')
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const customer = [...Array(20)].map(() => {
+      const address = `${falso.randStreetAddress()} ${falso.randCity()}, ${falso.randState()}`
+      return {
+        FirstName: falso.randFirstName(),
+        LastName: falso.randLastName(),
+        email: falso.randEmail(),
+        passwordDigest: falso.randPassword(),
+        address: address,
+        createdAt: falso.randPastDate(),
+        updatedAt: falso.randRecentDate()
+      }
+    })
+    await queryInterface.bulkInsert('customer', customer)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('customer')
+  }
+}
