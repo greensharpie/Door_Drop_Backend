59d6102ae8e523b4875b2cd60e583d8450a30ae5
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-11 16:12:30 -0500

    order post and get routes established

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -16,6 +16,38 @@ const getOrderItems = async (req, res) => {
   }
 }
 
+const GetOrdersById = async (req, res) => {
+  try {
+    const orders = await Order.findByPk(req.params.id, {
+      include: [{ model: MenuItem, as: 'order_items' }]
+    })
+    res.send(orders)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const AddOrderItem = async (req, res) => {
+  try {
+    const order_items = await MenuItem.findByPk(req.params.item_id)
+    const orderId = parseInt(req.params.order_id)
+    const restaurantId = parseInt(req.params.restaurant_id)
+
+    let orderBody = {
+      name: order_items.name,
+      price: order_items.price,
+      description: order_items.description,
+      image: order_items.image
+    }
+    const addItemToOrder = await MenuItem.create(orderBody)
+    res.send(addItemToOrder)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
 module.exports = {
-  getOrderItems
+  getOrderItems,
+  GetOrdersById,
+  AddOrderItem
 }
--- routes/OrderRouter.js
+++ routes/OrderRouter.js
@@ -2,5 +2,10 @@ const Router = require('express').Router()
 const controller = require('../controllers/OrderController')
 
 Router.get('/order_items', controller.getOrderItems)
+Router.get('/order_items/id/:id', controller.GetOrdersById)
+Router.post(
+  '/add_order_item/order_id/:order_id/restaurant_id/:restaurant_id/item_id/:item_id',
+  controller.AddOrderItem
+)
 
 module.exports = Router
