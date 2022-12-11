3df8c6b27c3cc2e3ec8a9609c68a5a6f9eb8209d
Kevin Blanco <kevinjblanco@gmail.com>
2022-12-11 14:44:31 -0500

    post and put requestfor restaurant routes functional

--- controllers/RestaurantController.js
+++ controllers/RestaurantController.js
@@ -16,6 +16,47 @@ const GetAllRestaurants = async (req, res) => {
   }
 }
 
+const GetRestaurantById = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.id, {
+      include: [
+        {
+          model: MenuItem,
+          as: 'restaurant_items'
+        }
+      ]
+    })
+    res.send(restaurant)
+  } catch (error) {
+    throw error
+  }
+}
+
+const CreateRestaurant = async (req, res) => {
+  try {
+    const newRest = await Restaurant.create(req.body)
+    res.send(newRest)
+  } catch (error) {
+    return res.status(500).send(error.message)
+  }
+}
+
+const UpdateRestaurant = async (req, res) => {
+  try {
+    let restaurantId = parseInt(req.params.restaurant_id)
+    let updatedRest = await Restaurant.update(req.body, {
+      where: { id: restaurantId },
+      returning: true
+    })
+    res.send(updatedRest)
+  } catch (error) {
+    throw error
+  }
+}
+
 module.exports = {
-  GetAllRestaurants
+  GetAllRestaurants,
+  GetRestaurantById,
+  CreateRestaurant,
+  UpdateRestaurant
 }
--- routes/RestaurantRouter.js
+++ routes/RestaurantRouter.js
@@ -2,5 +2,8 @@ const Router = require('express').Router()
 const controller = require('../controllers/RestaurantController')
 
 Router.get('/get_all', controller.GetAllRestaurants)
+Router.get('/id/:id', controller.GetRestaurantById)
+Router.post('/new_restaurant', controller.CreateRestaurant)
+Router.put('/id/:restaurant_id', controller.UpdateRestaurant)
 
 module.exports = Router
